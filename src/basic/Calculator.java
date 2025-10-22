c8a9b5dcee17c518c37df0be02f828e61b53536b
Nico Lee <new.nikolee2007@gmail.com>
2025-10-22 21:00:14 +0700

    update: fix

---
+++ src/basic/Calculator.java
@@ -0,0 +1,11 @@
+package basic;
+
+
+public class Calculator {
+    // Method untuk menjumlahkan dua bilangan
+    public int add(int a, int b) {
+        
+        int sum = a + b; // Menghitung jumlah dari a dan b
+        return sum; // Mengembalikan hasil penjumlahan
+    }
+}
